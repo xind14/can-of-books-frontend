628075f18b0f7b7aa72405f325ea4d9c598ba60d
JohnnyBackus <john.p.backus@gmail.com>
2023-12-11 22:18:34 -0800

    changed Nav Bar to have button functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+  // Routes,
+  // Route
+// } from "react-router-dom";
 
 function App () {
+
+  const [getBooks, setGetBooks] = useState(false);
+  const handleGetBooks = () => {
+    setGetBooks(true);
+  };
+
   return (
     <>
-      <Router>
-          <Header />
-          <Routes>
+      {/* <Router> */}
+          <Header handleGetBooks={handleGetBooks} />
+          <BestBooks getBooks={getBooks} />
+          {/* <Routes>
           <Route 
               exact path="/"
-              element={<BestBooks />}
+              element={<BestBooks getBooks={getBooks} />}
               >
-            </Route>
+            </Route> */}
             {/* <Route 
               exact path="/about"
               // element={< />}
@@ -31,9 +38,9 @@ function App () {
               >
             </Route> */}
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
-          </Routes>
+          {/* </Routes> */}
           <Footer />
-        </Router>
+        {/* </Router> */}
       </>
   )
 }
--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,39 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
-import { Alert } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
-let url = import.meta.env.VITE_LOCAL_SERVER
+let url = import.meta.env.VITE_LOCAL_SERVER;
 
-async function BestBooks() {
-  const [books, setBooks] = useState([]);
-  // if (books.length<1) {
-  //   return (Alert.danger('Book Collection is Empty'));
-  }
-  if (books.length>0) {
-    try {
-      let response = await axios.get(`${url}/books`);
-      setBooks(response.data);
-      // console.log(response.data);
-    } catch(error) {
-      setErrorMessage(error.message);
-    }
+function EmptyLibrary() {
+  const [showAlert, setShowAlert] = useState(true);
+  if (showAlert === true) {
     return (
-      <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>,
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-      {books.map((book, i) => {
-        <Carousel.Item>
-          <ExampleCarouselImage text="1st book" />
-          <Carousel.Caption>
-            <h3>{books[i].title}</h3>
-            <p>{books[i].description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        })
+      <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+        <Alert.Heading>Book Collection Empty</Alert.Heading>
+        <p>
+          Please use the menu above to add books to your collection.
+        </p>
+      </Alert>
+    );
+  }
+  return <Button onClick={() => setShowAlert(true)}>Show Alert</Button>;
+}
+
+function BestBooks(props) {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      if (props.getBooks) {
+        try {
+          let response = await axios.get(`${url}/books`);
+          setBooks(response.data);
+          console.log(response.data);
+        } catch(error) {
+          console.log(error.message);
+        }
       }
-        </Carousel>
-        )
-     }
-}        
+    }
+    fetchBooks();
+  }, [props.getBooks]);
+
+  if (books.length < 1) {
+    return <EmptyLibrary />;
+  }
+
+  return (
+    <>
+      <Carousel>
+        {/* <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2> */}
+        {books.map((book, i) => {
+          return(
+            <Carousel.Item key={books[i]._id}>
+              <Image src='/images/placeholder.png' alt='image of bookcover' />
+              <Carousel.Caption>
+                <h3>{books[i].title}</h3>
+                <p>{books[i].description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );           
+        })}
+      </Carousel>
+    </>
+  );
+}
 
 export default BestBooks;
